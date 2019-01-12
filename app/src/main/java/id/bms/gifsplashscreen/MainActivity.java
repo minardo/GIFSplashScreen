bc9cc7e479da9e74c979e7a44aaa59931aa20d50
minardo <ardo15.project@gmail.com>
2019-01-12 21:47:46 +0700

    Initial commit

---
+++ app/src/main/java/id/bms/gifsplashscreen/MainActivity.java
@@ -0,0 +1,26 @@
+package id.bms.gifsplashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(MainActivity.this, DashboardActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
